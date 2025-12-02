c2abe75adfa1a34ae7a5258756ee59dddfae3303
blizzardsnowstorm <mintiestleaf@outlook.com>
2025-12-01 23:12:39 -0500

    Login 'system' lol

--- usedpc.py
+++ usedpc.py
@@ -29,6 +29,7 @@ enterprompt()
 clearscreen()
 #boot
 print("[UEFI] Minimal Firmware Check V1.02")
+time.sleep(3)
 print("CPU: Intel Celeron N4000 @ 1.10 GHz (4MB cache)")
 slowprint("Testing RAM: 4GB DDR4 24000MHz ... [OK]")
 slowprint("HDD: Primary Master: 256GB SSD (SATA III) ... [OK]")
@@ -38,4 +39,18 @@ slowprint("Initializing disk cache ... [OK]")
 slowprint("Waking LAN adapter ... [OK]")
 slowprint("Starting Input Listener Service ... [RESTARTING]")
 slowprint("Running [R E A P E R] integrity check ... [DONE]")
+time.sleep(1)
+
+username = input("Create a username: ")
+password = input("create a password: ")
+p2 = input("confirm password: ")
+if p2 == password:
+    print("password confirmed")
+else:
+    print("incorrect password")
+
+
+
+
+
 
